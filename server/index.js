0f1dfc6d510fc24b94bf9fc430707092ab52c2b4
Michaelstarwin <michealstarwin323@gmail.com>
2026-03-18 20:05:15 +0530

    first commit

--- server/index.js
+++ server/index.js
@@ -64,6 +64,7 @@ app.use((req, res, next) => {
   next();
 });
 app.use(bodyParser.json());
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 // Ensure data directory and file exist
 const ensureDataFile = async () => {
